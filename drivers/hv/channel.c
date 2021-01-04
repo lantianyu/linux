dc4bff51abcf7f6f31f67ec284898388444ad341
Tianyu Lan <Tianyu.Lan@microsoft.com>
2021-01-04 07:30:40 -0800

    HV/Vmbus: Initialize VMbus ring buffer for Isolation VM
    VMbus ring buffer are shared with host and it's need to
    be accessed via extra address space of Isolation VM with
    SNP support. This patch is to map the ring buffer
    address in extra address space via ioremap(). HV host
    visibility hvcall smears data in the ring buffer and
    so reset the ring buffer memory to zero after calling
    visibility hvcall.
    
    Signed-off-by: Tianyu Lan <Tianyu.Lan@microsoft.com>

--- drivers/hv/channel.c
+++ drivers/hv/channel.c
@@ -712,6 +712,16 @@ static int __vmbus_open(struct vmbus_channel *newchannel,
 	if (err)
 		goto error_clean_ring;
 
+	err = hv_ringbuffer_post_init(&newchannel->outbound,
+				      page, send_pages);
+	if (err)
+		goto error_free_gpadl;
+
+	err = hv_ringbuffer_post_init(&newchannel->inbound,
+				      &page[send_pages], recv_pages);
+	if (err)
+		goto error_free_gpadl;
+
 	/* Create and init the channel open message */
 	open_info = kzalloc(sizeof(*open_info) +
 			   sizeof(struct vmbus_channel_open_channel),
