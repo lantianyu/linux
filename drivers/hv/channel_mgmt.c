9373aeedb76e63d069db2222471b2a5c96675b29
Weidong Cui <weidongcui@gmail.com>
2021-08-18 07:02:39 -0700

    move hv_init_channel_ivm before vmbus_device_register

--- drivers/hv/channel_mgmt.c
+++ drivers/hv/channel_mgmt.c
@@ -531,6 +531,10 @@ static void vmbus_add_channel_work(struct work_struct *work)
 		goto err_deq_chan;
 
 	newchannel->device_obj->device_id = newchannel->device_id;
+
+	if (hv_init_channel_ivm(newchannel))
+		goto err_deq_chan;
+
 	/*
 	 * Add the new device to the bus. This will kick off device-driver
 	 * binding which eventually invokes the device driver's AddDevice()
@@ -550,9 +554,6 @@ static void vmbus_add_channel_work(struct work_struct *work)
 	}
 
 	newchannel->probe_done = true;
-
-	if (hv_init_channel_ivm(newchannel))
-		goto err_deq_chan;
 
 	return;
 
