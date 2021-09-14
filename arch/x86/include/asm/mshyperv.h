08b064a38585b529d94bdab9a10694d5c750f388
Tianyu Lan <Tianyu.Lan@microsoft.com>
2021-09-14 09:02:25 -0400

    x86/hyperv: Add new hvcall guest address host visibility  support
    Add new hvcall guest address host visibility support to mark
    memory visible to host. Call it inside set_memory_decrypted
    /encrypted(). Add HYPERVISOR feature check in the
    hv_is_isolation_supported() to optimize in non-virtualization
    environment.
    
    Acked-by: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Tianyu Lan <Tianyu.Lan@microsoft.com>
    ---
    Change since v4:
            * Fix typo in the comment
    	* Make hv_mark_gpa_visibility() to be a static function
    	* Merge __hv_set_mem_host_visibility() and hv_set_mem_host_visibility()
    
    Change since v3:
    	* Fix error code handle in the __hv_set_mem_host_visibility().
    	* Move HvCallModifySparseGpaPageHostVisibility near to enum
    	  hv_mem_host_visibility.
    
    Change since v2:
           * Rework __set_memory_enc_dec() and call Hyper-V and AMD function
             according to platform check.
    
    Change since v1:
           * Use new staic call x86_set_memory_enc to avoid add Hyper-V
             specific check in the set_memory code.

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -192,7 +192,7 @@ struct irq_domain *hv_create_pci_msi_domain(void);
 int hv_map_ioapic_interrupt(int ioapic_id, bool level, int vcpu, int vector,
 		struct hv_interrupt_entry *entry);
 int hv_unmap_ioapic_interrupt(int ioapic_id, struct hv_interrupt_entry *entry);
-
+int hv_set_mem_host_visibility(unsigned long addr, int numpages, bool visible);
 #else /* CONFIG_HYPERV */
 static inline void hyperv_init(void) {}
 static inline void hyperv_setup_mmu_ops(void) {}
