c0ca8d0a47833108744747b3d5877efe0ff2e387
Tianyu Lan <tiala@microsoft.com>
2022-11-05 04:19:53 -0400

    clocksource: hyper-v: decrypt hyperv tsc page in sev-snp enlightened guest
    Hyper-V tsc page is shared with hypervisor and it should be decrypted
    in sev-snp enlightened guest when it's used.
    
    Signed-off-by: Tianyu Lan <tiala@microsoft.com>

--- drivers/clocksource/hyperv_timer.c
+++ drivers/clocksource/hyperv_timer.c
@@ -365,7 +365,7 @@ EXPORT_SYMBOL_GPL(hv_stimer_global_cleanup);
 static union {
 	struct ms_hyperv_tsc_page page;
 	u8 reserved[PAGE_SIZE];
-} tsc_pg __aligned(PAGE_SIZE);
+} tsc_pg __bss_decrypted __aligned(PAGE_SIZE);
 
 struct ms_hyperv_tsc_page *hv_get_tsc_page(void)
 {
