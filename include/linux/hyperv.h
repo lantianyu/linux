769ad40eba84fc631844f9b0e5534e343c55e5d1
Tianyu Lan <tiala@microsoft.com>
2023-02-04 02:15:33 -0500

    Revert "scsi: storvsc: Add Isolation VM support for storvsc driver"
    This reverts commit 4f228833e44ba47d476ecafd5b4417695f049c34.

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -1296,7 +1296,6 @@ struct hv_device {
 
 	struct vmbus_channel *channel;
 	struct kset	     *channels_kset;
-	struct device_dma_parameters dma_parms;
 
 	/* place holder to keep track of the dir for hv device in debugfs */
 	struct dentry *debug_dir;
