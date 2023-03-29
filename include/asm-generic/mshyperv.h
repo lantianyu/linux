f9ff6224f8b84223d2c25798d5a22e3c31f3314f
Tianyu Lan <tiala@microsoft.com>
2023-03-28 23:54:52 -0400

    x86/hyperv: Add sev-snp enlightened guest static key
    Introduce static key isolation_type_en_snp for enlightened
    sev-snp guest check.
    
    Signed-off-by: Tianyu Lan <tiala@microsoft.com>

--- include/asm-generic/mshyperv.h
+++ include/asm-generic/mshyperv.h
@@ -36,15 +36,21 @@ struct ms_hyperv_info {
 	u32 nested_features;
 	u32 max_vp_index;
 	u32 max_lp_index;
-	u32 isolation_config_a;
+	union {
+		u32 isolation_config_a;
+		struct {
+			u32 paravisor_present : 1;
+			u32 reserved_a1 : 31;
+		};
+	};
 	union {
 		u32 isolation_config_b;
 		struct {
 			u32 cvm_type : 4;
-			u32 reserved1 : 1;
+			u32 reserved_b1 : 1;
 			u32 shared_gpa_boundary_active : 1;
 			u32 shared_gpa_boundary_bits : 6;
-			u32 reserved2 : 20;
+			u32 reserved_b2 : 20;
 		};
 	};
 	u64 shared_gpa_boundary;
