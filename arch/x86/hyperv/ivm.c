4dbc1273a1de88f1ba7961c7d342dc8a366bd57a
Tianyu Lan <tiala@microsoft.com>
2023-03-29 23:45:38 -0400

    x86/hyperv: SEV-SNP enlightened guest don't support legacy rtc
    SEV-SNP enlightened guest doesn't support legacy rtc. Set
    legacy.rtc, x86_platform.set_wallclock and get_wallclock to
    0 or noop(). Make get/set_rtc_noop() to be public and reuse
    them in the ms_hyperv_init_platform().
    
    Signed-off-by: Tianyu Lan <tiala@microsoft.com>

--- arch/x86/hyperv/ivm.c
+++ arch/x86/hyperv/ivm.c
@@ -391,6 +391,9 @@ __init void hv_sev_init_mem_and_cpu(void)
 	 * bios regions and reserve resources are not
 	 * available. Set these callback to NULL.
 	 */
+	x86_platform.legacy.rtc = 0;
+	x86_platform.set_wallclock = set_rtc_noop;
+	x86_platform.get_wallclock = get_rtc_noop;
 	x86_platform.legacy.reserve_bios_regions = 0;
 	x86_init.resources.probe_roms = x86_init_noop;
 	x86_init.resources.reserve_resources = x86_init_noop;
