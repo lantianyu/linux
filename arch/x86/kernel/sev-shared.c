95aa7e459ae9259a24e5b2911e5a98467f3196d5
Tianyu Lan <tiala@microsoft.com>
2023-04-03 12:40:37 -0400

    cpuid workaround

--- arch/x86/kernel/sev-shared.c
+++ arch/x86/kernel/sev-shared.c
@@ -556,8 +556,14 @@ snp_cpuid(struct ghcb *ghcb, struct es_em_ctxt *ctxt, struct cpuid_leaf *leaf)
 		/* Skip post-processing for out-of-range zero leafs. */
 		if (!(leaf->fn <= RIP_REL_REF(cpuid_std_range_max) ||
 		      (leaf->fn >= 0x40000000 && leaf->fn <= RIP_REL_REF(cpuid_hyp_range_max)) ||
-		      (leaf->fn >= 0x80000000 && leaf->fn <= RIP_REL_REF(cpuid_ext_range_max))))
-			return 0;
+		      (leaf->fn >= 0x80000000 && leaf->fn <= RIP_REL_REF(cpuid_ext_range_max)))) {
+
+			if (leaf->fn < 0x400000ff && leaf->fn >= 0x40000000) {
+				return -EOPNOTSUPP;
+			} else {
+				return 0;
+			}
+		}
 	}
 
 	return snp_cpuid_postprocess(ghcb, ctxt, leaf);
