02d8093b1d0841d51e2e1f8cd42efe1da4987794
Roman Kiselev 🦇 <romank@microsoft.com>
2023-04-26 21:04:52 +0000

    Merged PR 8011958: Build and publish the next package, too
    This way we can ingest every package as the production one or the `Next` one to mix and match inside the Hvlite repository what is deployed as the production firmware and what is the next best thing to happen to Underhill :) Some updates to the Hvlite pipeline will be needed, too, to build and put the `underhill-next.bin` firmware into the vpack next to the production `underhill.bin` one.
    
    ```
    Pushing Microsoft.HyperV.Internal.HCL.x64.1.0.2306.216-20230425.nupkg to 'https://msazure.pkgs.visualstudio.com/_packaging/af13a45c-5416-7db3-a830-8e591b213c6c/nuget/v2/'...
         .....
    Your package was pushed.
    
    ......
    
    Pushing Microsoft.HyperV.Internal.HCL.x64.Next.1.0.2306.216-20230425-next.nupkg to 'https://msazure.pkgs.visualstudio.com/_packaging/af13a45c-5416-7db3-a830-8e591b213c6c/nuget/v2/'...
         .....
    Your package was pushed.
    
    ```
    
    https://msazure.visualstudio.com/LSG-linux/_build/results?buildId=72310357&view=logs&j=3944e4b6-93b2-5963-f9b9-99d3388dd26b&t=b48dd2f2-e043-5037-7568-133a9eafabd1
    ![image.png](https://msazure.visualstudio.com/6e09f0eb-e532-4615-98ab-afa7a6220e98/_apis/git/repositories/82197ee4-cbe1-4fed-bb27-d2d74d9fc22d/pullRequests/8011958/attachments/image.png)

--- Microsoft/generate-nuspec.sh
+++ Microsoft/generate-nuspec.sh
@@ -2,7 +2,12 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-xmlstarlet ed -s '/package/metadata' -t elem -n version -v "${BUILD_BUILDNUMBER}-$(date +%Y%m%d)-dev" ${SCRIPT_DIR}/hcl.nuspec |\
+xmlstarlet ed -s '/package/metadata' -t elem -n version -v "${BUILD_BUILDNUMBER}-$(date +%Y%m%d)" ${SCRIPT_DIR}/hcl.nuspec |\
 xmlstarlet ed -s '/package/metadata' -t elem -n releaseNotes -v "${BUILD_SOURCEVERSION}: ${BUILD_SOURCEVERSIONMESSAGE}" |\
 xmlstarlet ed -s '/package/files/file' -t attr -n src -v "${OB_OUTPUTDIRECTORY}/**" > ${BUILD_SOURCESDIRECTORY}/hcl.nuspec
 cat ${BUILD_SOURCESDIRECTORY}/hcl.nuspec
+
+xmlstarlet ed -s '/package/metadata' -t elem -n version -v "${BUILD_BUILDNUMBER}-$(date +%Y%m%d)-next" ${SCRIPT_DIR}/hcl-next.nuspec |\
+xmlstarlet ed -s '/package/metadata' -t elem -n releaseNotes -v "${BUILD_SOURCEVERSION}: ${BUILD_SOURCEVERSIONMESSAGE}" |\
+xmlstarlet ed -s '/package/files/file' -t attr -n src -v "${OB_OUTPUTDIRECTORY}/**" > ${BUILD_SOURCESDIRECTORY}/hcl-next.nuspec
+cat ${BUILD_SOURCESDIRECTORY}/hcl-next.nuspec
