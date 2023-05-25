7113ba35ed40684664ef3d3f44c9943d2696595a
Tianyu Lan <tiala@microsoft.com>
2023-05-25 04:05:52 -0400

    x86/sev: Add restricted interrupt injection flag in SNP present features
    Enabled restrict interrupt injection flag in the SNP_
    FEATURES_PRESENT to let kernel boot up with this feature
    otherwise guest will terminate itself.

--- arch/x86/boot/compressed/sev.c
+++ arch/x86/boot/compressed/sev.c
@@ -375,7 +375,7 @@ static void enforce_vmpl0(void)
  * by the guest kernel. As and when a new feature is implemented in the
  * guest kernel, a corresponding bit should be added to the mask.
  */
-#define SNP_FEATURES_PRESENT	MSR_AMD64_SNP_DEBUG_SWAP
+#define SNP_FEATURES_PRESENT	MSR_AMD64_SNP_DEBUG_SWAP | MSR_AMD64_SNP_RESTRICTED_INJ
 
 u64 snp_get_unsupported_features(u64 status)
 {
